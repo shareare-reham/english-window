a3266fbcc852a71c6ddf703418e080348e207514
riham5477 <golamshahrearereham12@gmail.com>
2026-08-07 17:05:30 +0600

    implement dynamic details word modal

--- scripts/index.js
+++ scripts/index.js
@@ -14,7 +14,7 @@ function displayLessons(lessons) {
   for (const lesson of lessons) {
     const btnDiv = document.createElement("div");
     btnDiv.innerHTML = `
-        <button onclick="wordsLoad(${lesson.level_no})" class="btn btn-outline btn-primary">
+        <button id="active-btn-${lesson.level_no}" onclick="wordsLoad(${lesson.level_no})" class="lesson-btn btn btn-outline btn-primary">
         <i class="fa-solid fa-book-open" style="color: #422ad5"></i>
             Lesson-${lesson.level_no}
         </button>
@@ -24,8 +24,19 @@ function displayLessons(lessons) {
 }
 lessonLoad();
 
+const removeActive = () => {
+  const activeBtns = document.querySelectorAll(".lesson-btn");
+  for (const btn of activeBtns) {
+    btn.classList.remove("active");
+  }
+};
+
 const wordsLoad = (lesson) => {
   const url = `https://openapi.programming-hero.com/api/level/${lesson}`;
+  const activeBtn = document.getElementById(`active-btn-${lesson}`);
+  removeActive();
+  activeBtn.classList.add("active");
+
   fetch(url)
     .then((response) => response.json())
     .then(function (json) {
@@ -47,15 +58,15 @@ const displayWords = (words) => {
     card.innerHTML = `
         <div class="bg-white p-10 rounded-sm h-full flex flex-col justify-between mb-10">
           <div class="text-center items-center">
-            <h2 class="font-bold text-3xl">${word.word}</h2>
+            <h2 class="font-bold text-3xl">${word.word ? word.word : "Word Not Found"}</h2>
             <p class="font-medium text-xl my-6">Meaning / Pronounciation</p>
             <h2 class="bangla text-[#18181B] font-semibold text-3xl mb-14">
-              "${word.meaning} / ${word.pronunciation}"
+              "${word.meaning ? word.meaning : "Meaning Not Found"} / ${word.pronunciation ? word.pronunciation : "Pronunciation Not Found"}"
             </h2>
           </div>
           <div class="flex justify-between items-center">
             <div class="bg-[#1A91FF]/10 p-3 rounded-sm hover:bg-[#1A91FF]/80">
-              <i class="fa-solid fa-circle-info" style="color: #374957"></i>
+              <button onclick="detailsLoad(${word.id})"><i class="fa-solid fa-circle-info" style="color: #374957"></i></button>
             </div>
             <div class="bg-[#1A91FF]/10 p-3 rounded-sm hover:bg-[#1A91FF]/80">
               <i class="fa-solid fa-volume" style="color: #374957"></i>
@@ -84,3 +95,51 @@ const displayNoVocub = () => {
       </div>
   `;
 };
+
+const detailsLoad = (id) => {
+  const url = `https://openapi.programming-hero.com/api/word/${id}`
+  fetch(url)
+  .then((response) => response.json())
+  .then((json) => showDetails(json.data))
+};
+
+const showDetails =(datas)=>{
+ 
+  const modal = document.getElementById("my_modal_5");
+  modal.innerHTML = `
+        <div class="modal-box rounded-2xl">
+          <div class="m-3 p-4 border-2 border-[#EDF7FF] space-y-8 rounded-xl">
+            <h1 class="font-semibold text-3xl">${datas.word} (<i class="fa-solid fa-microphone-lines"></i>: ${datas.pronunciation})</h1>
+            <div>
+              <h3 class="font-semibold text-xl mb-2">Meaning</h3>
+              <p class="bangla font-medium text-xl">${datas.meaning}</p>
+            </div>
+            <div>
+              <h3 class="font-semibold text-xl mb-2">Example</h3>
+              <p class="text-xl">${datas.sentence}</p>
+            </div>
+            <div>
+              <h3 class="bangla font-medium text-xl mb-2">সমার্থক শব্দ গুলো</h3>
+              <div class="flex gap-4">
+                <div class="bg-[#D7E4EF] p-2 rounded-lg">
+                  <p>${datas.synonyms[0]}</p>
+                </div>
+                <div class="bg-[#D7E4EF] p-2 rounded-lg">
+                  <p>${datas.synonyms[1]}</p>
+                </div>
+                <div class="bg-[#D7E4EF] p-2 rounded-lg">
+                  <p>${datas.synonyms[2]}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="modal-action justify-start">
+            <form method="dialog">
+              <!-- if there is a button in form, it will close the modal -->
+              <button class="btn btn-active btn-primary rounded-xl">Complete Learning</button>
+            </form>
+          </div>
+        </div>
+  `;
+  my_modal_5.showModal()
+}
